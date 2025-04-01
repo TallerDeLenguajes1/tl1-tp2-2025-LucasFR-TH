a1e09d6e164cfae17c619b6c48860d6cce756ed6
LucasFR-TH <lucas.f.roldan@gmail.com>
2025-04-01 16:06:34 -0300

    Cambio el codigo para usar punteros

---
+++ tp2_1_2.c
@@ -0,0 +1,17 @@
+// codigo a completar
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 20
+
+int main() {
+    srand(time(NULL));
+    double *punt;
+    double vt[N];
+    
+    for(punt = vt; punt<vt+N; punt++) {
+        *punt=1+rand()%100;
+        printf("%f\n", *punt);
+    }
+}
